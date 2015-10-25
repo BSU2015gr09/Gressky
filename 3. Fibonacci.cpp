d7f07172e4dcafca33be7e1f225bb4a98d7127c1
NickBright <gressssssssky@gmail.com>
2015-10-25 17:21:54 +0300

    Îòíîøåíèÿ ÷èñåë Ôèáîíà÷÷è

---
+++ 3. Fibonacci.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <clocale>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float mas[39], a, b;
+	int n;
+	cout << "                         Отношение                         " << endl;
+	cout << "Последующего к предыдущему   |    Предыдущего к последующему" << endl;
+	cout << "                   1         |          1                  " << endl;
+	mas[0] = 1;
+	mas[1] = 1;
+	for (n = 2; n < 39; n++){
+		mas[n] = mas[n - 1] + mas[n - 2];
+		a = mas[n] / mas[n - 1];
+		b = mas[n - 1] / mas[n];
+		cout << setw(20) << setprecision(5) << a;
+		cout << setw(10) << "|";
+		cout << setw(10) << fixed << setprecision(5) << " " << b << endl;}
+	return 0;}
