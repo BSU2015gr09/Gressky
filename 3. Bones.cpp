43d1880f4832bb21791585b786b188d6e6a193c1
NickBright <gressssssssky@gmail.com>
2015-10-25 11:04:43 +0300

    Bones and caclulator

---
+++ 3. Bones.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <clocale>
+using std::cout;
+using std::cin;
+using std::endl;
+int main() {
+	int a = 0, b = 0, c = 0, res1 = 0, res2 = 0;
+	setlocale(LC_ALL, "Russian");
+	cout << "Добро пожаловать в Кости! Победитель тот, у кого выпадет больше очков" << endl;
+	while (1 > 0) {cout << "Для игры введите любое число: ";
+			cin >> c;
+			a = 1 + rand() % 6;
+			b = 1 + rand() % 6;
+			switch (a) {
+			case 1: cout << "-----" << endl << "--o--" << endl << "-----" << endl; break;
+			case 2: cout << "-----" << endl << "-o-o-" << endl << "-----" << endl; break;
+			case 3: cout << "o----" << endl << "--o--" << endl << "----o" << endl; break;
+			case 4: cout << "--o--" << endl << "-o-o-" << endl << "--o--" << endl; break;
+			case 5: cout << "--o--" << endl << "o-o-o" << endl << "--o--" << endl; break;
+			case 6: cout << "-o-o-" << endl << "-o-o-" << endl << "-o-o-" << endl; break;}
+			cout << endl;
+			switch (b) {
+			case 1: cout << "-----" << endl << "--o--" << endl << "-----" << endl; break;
+			case 2: cout << "-----" << endl << "-o-o-" << endl << "-----" << endl; break;
+			case 3: cout << "o----" << endl << "--o--" << endl << "----o" << endl; break;
+			case 4: cout << "--o--" << endl << "-o-o-" << endl << "--o--" << endl; break;
+			case 5: cout << "--o--" << endl << "o-o-o" << endl << "--o--" << endl; break;
+			case 6: cout << "-o-o-" << endl << "-o-o-" << endl << "-o-o-" << endl; break;}
+			res1 = a + b;
+			cout << endl << "Ваш результат: " << res1 << endl;
+			a = 1 + rand() % 6;
+			b = 1 + rand() % 6;
+			switch (a) {
+			case 1: cout << "-----" << endl << "--o--" << endl << "-----" << endl; break;
+			case 2: cout << "-----" << endl << "-o-o-" << endl << "-----" << endl; break;
+			case 3: cout << "o----" << endl << "--o--" << endl << "----o" << endl; break;
+			case 4: cout << "--o--" << endl << "-o-o-" << endl << "--o--" << endl; break;
+			case 5: cout << "--o--" << endl << "o-o-o" << endl << "--o--" << endl; break;
+			case 6: cout << "-o-o-" << endl << "-o-o-" << endl << "-o-o-" << endl; break;}
+			cout << endl;
+			switch (b) {
+			case 1: cout << "-----" << endl << "--o--" << endl << "-----" << endl; break;
+			case 2: cout << "-----" << endl << "-o-o-" << endl << "-----" << endl; break;
+			case 3: cout << "o----" << endl << "--o--" << endl << "----o" << endl; break;
+			case 4: cout << "--o--" << endl << "-o-o-" << endl << "--o--" << endl; break;
+			case 5: cout << "--o--" << endl << "o-o-o" << endl << "--o--" << endl; break;
+			case 6: cout << "-o-o-" << endl << "-o-o-" << endl << "-o-o-" << endl; break;}
+			res2 = a + b;
+			cout << endl << "Результат компьютера: " << res2 << endl;
+			if (res1 > res2) { cout << "Вы победили!" << endl; }
+			if (res1 == res2) { cout << "Ничья!" << endl; }
+			if (res1 < res2) { cout << "Вы проиграли!" << endl; }}
+	return 0;}
---
+++ 3. Calculator.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <clocale>
+using std::cout;
+using std::cin;
+int main()
+{	 float z = 0, x = 0, y = 0, C=0, res;
+	 char oper = 0;
+	setlocale(LC_ALL, "Russian");
+	while (1 > 0) {
+		if (C == 0) {
+			cout << "Введите первое число: ";
+			cin >> x;
+			cout << "Введите операцию (для выхода введите #, для сброса введите c: ";
+			cin >> oper;
+			if (oper != '#') {cout << "Введите второе число: ";
+							  cin >> y;}
+			calc:
+			switch (oper) {
+			case '+': z = x + y;
+				cout << "Результат: " << z << "\n";
+				x = z;
+				C = 1;
+				break;
+			case '-': z = x - y;
+				cout << "Результат: " << z << "\n";
+				x = z;
+				C = 1;
+				break;
+			case '*': z = x * y;
+				cout << "Результат: " << z << "\n";
+				x = z;
+				C = 1;
+				break;
+			case '/': z = x / y;
+				cout << "Результат: " << z << "\n";
+				x = z;
+				C = 1;
+				break;
+			case '#': goto end;
+				break;
+			case 'c': C = 0;
+				cout << "Выражение обнулилось "<<"\n";
+				break;}}
+		else {cout << "Введите операцию (для выхода введите #, для сброса введите c): ";
+			  cin >> oper;
+			  if (oper != '#' && oper != 'c') { cout << "Введите второе число: ";
+								 cin >> y;}
+		      goto calc;}}
+	end:
+	return 0;
+}
