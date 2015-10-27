c6e14769740368b76b1361e8d9f3ccb66b449e31
barvenov <barvenovsa@gmail.com>
2015-10-27 11:57:01 +0400

    Замечания по коду...

--- 3. Random.cpp
+++ 3. Random.cpp
@@ -7,8 +7,8 @@ using std::setw;
 int main(){
 long int a = 4096, c = 150889, m = 714025, i = 0;
 int const N = 20;
-long long int mas[N];
-mas[0] = 1;
+long long int mas[N]; //массив??????
+mas[0] = 1; 
 cout << " 1 = " << mas[0] << "\n";
 for (i = 1; i<N; i++) {mas[i] = (a*mas[i - 1] + c) % m;
 		       cout << setw(2) << i + 1 << " = " << mas[i] << "\n";}
