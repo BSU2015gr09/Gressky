9987cf26b8f589b877f88a4c5e765ed8f7d310ba
NickBright <gressssssssky@gmail.com>
2015-11-01 22:53:17 +0300

    DZ 4

---
+++ 4. RandomMassive.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <clocale>
+#include <ctime>
+using std::cout;
+void swap(int A[], int a, int b) {
+	int tmp = A[a];
+	A[a] = A[b];
+	A[b] = tmp;}
+int main() {
+	int const N = 100;
+	int A[N], sum = 0, sum1 = 0, i, max, min, B[N];
+	setlocale(LC_ALL, "Russian");
+	srand(time(0));
+	cout << "Начальный массив: ";
+	for (i = 0; i < N; i++) {
+		B[i] = 1;
+		A[i] = rand() % 101 - 50;
+		cout << A[i] << ", ";
+	}
+	cout << "\n" << "Массив в обратном порядке: ";
+	for (i = 0; i < N / 2; i++) { swap(A, i, N - i - 1); }
+	for (i = 0; i < N; i++) { cout << A[i] << ", "; }
+	cout << "\n" << "Элементы на нечетных позициях: ";
+	for (i = 0; i < N; i = i + 2) { cout << A[i] << ", "; }
+	cout << "\n" << "Элементы на четных позициях: ";
+	for (i = 1; i < N; i = i + 2) { cout << A[i] << ", "; }
+	for (i = 0; i < N; i++) {
+		if (A[i] <= 0) { sum++; }
+		else { sum1++; }
+	}
+	cout << "\n" << "Количество отрицательных элементов: " << sum << "\n" << "Количество положительных элементов: " << sum1 << "\n";
+	sum = 0; sum1 = 0; max = A[0]; min = A[0];
+	for (i = 0; i < N; i++) {
+		if (A[i] > max) { max = A[i]; }
+		if (A[i] <= min) { min = A[i]; }
+	}
+	cout << "Максимальный элемент: " << max<<  "\n";
+	cout << "Минимальный элемент: " << min << "\n";
+}
---
+++ 4. XORShift.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <clocale>
+using std::cout;
+void XORShift(int &x){
+	int tmp, a = 13, b = 15, c = 5;
+	tmp = x ^ (x << a);
+	tmp = tmp ^ (tmp >> b);
+	x = tmp ^ (tmp << c);}
+int main(){
+	setlocale(LC_ALL, "Russian");
+	int x = 4;
+	XORShift(x);
+	cout << "Полученное псевдослучайное число: " << x << "\n";
+	return 0;}
