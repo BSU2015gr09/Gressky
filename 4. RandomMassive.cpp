1e4172b1ff078787f0063faf4c1b68d600524fcf
NickBright <gressssssssky@gmail.com>
2015-11-02 10:56:26 +0300

    I'm sorry, but I haven't done it with the use of functions. This variant is right

--- 4. RandomMassive.cpp
+++ 4. RandomMassive.cpp
@@ -6,34 +6,48 @@ void swap(int A[], int a, int b) {
 	int tmp = A[a];
 	A[a] = A[b];
 	A[b] = tmp;}
-int main() {
-	int const N = 100;
-	int A[N], sum = 0, sum1 = 0, i, max, min, B[N];
-	setlocale(LC_ALL, "Russian");
+void intialization(int A[], int N) {
 	srand(time(0));
 	cout << "Начальный массив: ";
-	for (i = 0; i < N; i++) {
-		B[i] = 1;
+	for (int i = 0; i < N; i++) {
 		A[i] = rand() % 101 - 50;
 		cout << A[i] << ", ";
 	}
+}
+void reverse(int A[], int N) {
 	cout << "\n" << "Массив в обратном порядке: ";
-	for (i = 0; i < N / 2; i++) { swap(A, i, N - i - 1); }
-	for (i = 0; i < N; i++) { cout << A[i] << ", "; }
+	for (int i = 0; i < N / 2; i++) { swap(A, i, N - i - 1); }
+	for (int i = 0; i < N; i++) { cout << A[i] << ", "; }
+}
+void parity(int A[], int N) {
 	cout << "\n" << "Элементы на нечетных позициях: ";
-	for (i = 0; i < N; i = i + 2) { cout << A[i] << ", "; }
+	for (int i = 0; i < N; i = i + 2) { cout << A[i] << ", "; }
 	cout << "\n" << "Элементы на четных позициях: ";
-	for (i = 1; i < N; i = i + 2) { cout << A[i] << ", "; }
-	for (i = 0; i < N; i++) {
+	for (int i = 1; i < N; i = i + 2) { cout << A[i] << ", "; }
+}
+void posneg(int A[], int N) {
+	int sum = 0, sum1 = 0;
+	for (int i = 0; i < N; i++) {
 		if (A[i] <= 0) { sum++; }
-		else { sum1++; }
-	}
+		else { sum1++; }}
 	cout << "\n" << "Количество отрицательных элементов: " << sum << "\n" << "Количество положительных элементов: " << sum1 << "\n";
-	sum = 0; sum1 = 0; max = A[0]; min = A[0];
-	for (i = 0; i < N; i++) {
+}
+void maxmin(int A[], int N) {
+	int  max = A[0], min = A[0];
+	for (int i = 0; i < N; i++) {
 		if (A[i] > max) { max = A[i]; }
 		if (A[i] <= min) { min = A[i]; }
 	}
-	cout << "Максимальный элемент: " << max<<  "\n";
+	cout << "Максимальный элемент: " << max << "\n";
 	cout << "Минимальный элемент: " << min << "\n";
+}
+int main() {
+	int const N = 100;
+	int A[N];
+	setlocale(LC_ALL, "Russian");
+	intialization(A, N);
+	reverse(A, N);
+	parity(A, N);
+	posneg(A, N);
+	maxmin(A, N);
 }
